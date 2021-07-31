0eb56994028b762ba5df15e115bd6c46342fc63b
lumilitas <lucasmengineer@edu.unifor.br>
2021-07-31 17:01:23 -0300

    Início da criação do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { 
+  Container,
+  Title,
+} from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
+
+export default Home;
