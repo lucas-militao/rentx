442bec60b0b40e29d13536239d47aba6474f2d95
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-02 17:56:01 -0300

    Implementado o tema global, criação do Header da Home

--- App.tsx
+++ App.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
 import Home from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/styles/theme';
+import { Text } from 'react-native';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider
+      theme={theme}
+    >
+      <Home />
+    </ThemeProvider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
 
 import { 
   Container,
-  Title,
+  Header,
+  HeaderContent,
+  TotalCars
 } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
