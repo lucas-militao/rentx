1398f8fdbe85cfe4910124724d6c697b83388e79
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-03 14:30:59 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import { 
   Container,
@@ -12,6 +13,26 @@ import {
 } from './styles';
 
 const Home: React.FC = () => {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  };
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://i.pinimg.com/originals/b3/12/53/b31253a815e632ef08ffb569e79436b8.png'
+  };
+
   return (
     <Container>
       <StatusBar
@@ -31,6 +52,9 @@ const Home: React.FC = () => {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
     </Container>
   );
 }
