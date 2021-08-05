da46605667825f389a7e30d300919dc5606b79ae
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-05 17:38:26 -0300

    Implementando a interface de Details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -6,7 +6,16 @@ import { ImageSlider } from "../../components/ImageSlider";
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
 } from './styles';
 
 export const CarDetails: React.FC = () => {
@@ -21,6 +30,27 @@ export const CarDetails: React.FC = () => {
           imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
         />
       </CarImages>
+
+      <Content>
+          <Details>
+            <Description>
+              <Brand>Lamborghini</Brand>
+              <Name>Huracan</Name>
+            </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário 
+          touro de lide indultado na praça Real 
+          Maestranza de Sevilla. É um belíssimo carro 
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   )
 }
