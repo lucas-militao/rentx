4380b7fe03762b8a82e12d12a4f5b21c6e75565b
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-23 17:31:38 -0300

    Segundo Animação com Splash

--- src/screens/Splash/indext.tsx
+++ src/screens/Splash/indext.tsx
@@ -20,36 +20,48 @@ export function Splash() {
   const brandStyle = useAnimatedStyle(() => {
     return {
       opacity: interpolate(
-        splashAnimation.value, 
-        [0, 25, 50], 
-        [1, .3, 0],
-        Extrapolate.CLAMP
-      ),
+        splashAnimation.value, [0, 25, 50], [1, .3, 0],),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP
+          )
+        }
+      ],
     }
   });
 
   const logoStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(
-        splashAnimation.value, 
-        [0, 25, 50], 
-        [0, .3, 1],
-        Extrapolate.CLAMP
-      ),
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, .3, 1]),
+      transform: [
+        {
+          translateX: interpolate(splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP  
+          )
+        }
+      ],
     }
   });
 
   useEffect(() => {
-    splashAnimation.value = withTiming( 50, { duration: 5000 })
+    splashAnimation.value = withTiming( 
+      50, 
+      { duration: 1000 }
+    );
   }, []);
 
   return(
     <Container>
-      <Animated.View style={brandStyle}>
+      <Animated.View style={[brandStyle, {position: 'absolute'}]}>
         <BrandSvg width={80} height={50}/>
       </Animated.View>
 
-      <Animated.View style={logoStyle}>
+      <Animated.View style={[logoStyle, {position: 'absolute'}]}>
         <LogoSvg width={180} height={20}/>
       </Animated.View>
     </Container>
