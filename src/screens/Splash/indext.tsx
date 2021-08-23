33aeba2e4efde41cee6d87780233ab689b41bc26
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-23 15:53:51 -0300

    Primeira animação

---
+++ src/screens/Splash/indext.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, StyleSheet } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+
+import {
+  Container
+} from './styles'
+
+export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: animation.value }
+      ]
+    }
+  })
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 500;
+  }
+
+  return(
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+
+      <Button
+        title="Mover"
+        onPress={handleAnimationPosition}
+      />
+    </Container>
+  )
+}
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
