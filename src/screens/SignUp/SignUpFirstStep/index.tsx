b845143106108607f7b69a1751a937865ee52428
lumilitas <lucasmengineer@edu.unifor.br>
2021-08-31 21:08:41 -0300

    Implementada a validação de cadastro da segunda tela do cadastro

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Keyboard, KeyboardAvoidingView } from "react-native";
+import React, { useState } from "react";
+import { Alert, Keyboard, KeyboardAvoidingView } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { Button } from "../../../components/Button";
 import { Input } from "../../../components/Input";
+import * as Yup from 'yup';
 
 import { 
   Container,
@@ -18,14 +19,37 @@ import {
 } from './styles';
 
 export function SignUpFirstStep() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [driverLicense, setDriverLicense] = useState('');
+
   const navigation = useNavigation();
 
   function handleBack() {
     navigation.goBack();
   }
 
-  function handleNextStep() {
-    navigation.navigate('SignUpSecondStep');
+  async function handleNextStep() {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('E-mail inválido'),
+        driverLicense: Yup.string()
+          .required('CNH é obrigatória')
+      });
+
+      const data = { name, email, driverLicense };
+      await schema.validate(data);
+
+      navigation.navigate('SignUpSecondStep', { user: data });
+    } catch (error) {
+      if(error instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', error.message);
+      }
+    }
   }
   
   return (
@@ -53,16 +77,22 @@ export function SignUpFirstStep() {
             <Input
               iconName="user"
               placeholder="Nome"
+              onChangeText={setName}
+              value={name}
             />
             <Input
               iconName="mail"
               placeholder="E-mail"
               keyboardType="email-address"
+              onChangeText={setEmail}
+              value={email}
             />
             <Input
               iconName="credit-card"
               placeholder="CNH"
               keyboardType="numeric"
+              onChangeText={setDriverLicense}
+              value={driverLicense}
             />
           </Form>
 
--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,12 +1,12 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import React, { useState } from "react";
+import { Alert } from "react-native";
 import { Keyboard, KeyboardAvoidingView } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { useTheme } from "styled-components";
 import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { Button } from "../../../components/Button";
-import { Input } from "../../../components/Input";
 import { PasswordInput } from "../../../components/PasswordInput";
 
 import { 
@@ -19,14 +19,37 @@ import {
   FormTitle,
 } from './styles';
 
-export function SignUpSecondStep() {
-  const theme = useTheme();
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driverLicense: string;
+  }
+}
 
+export function SignUpSecondStep() {
+  const [password, setPassowrd] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  const theme = useTheme();
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const { user } = route.params as Params;
 
   function handleBack() {
     navigation.goBack();
-  }  
+  } 
+
+  function handleRegister() {
+    if (!password || !passwordConfirm) {
+      return Alert.alert('Informe a senha e a confirmação dela');
+    }
+
+    if (password !== passwordConfirm) {
+      return Alert.alert('As senhas não são iguais');
+    }
+  }
   
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -53,17 +76,22 @@ export function SignUpSecondStep() {
             <PasswordInput
               iconName="lock"
               placeholder="Senha"
+              onChangeText={setPassowrd}
+              value={password}
             />
 
             <PasswordInput
               iconName="lock"
               placeholder="Repertir senha"
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
             />
           </Form>
 
           <Button
             color={theme.colors.success}
             title="Cadastrar"
+            onPress={handleRegister}
           />
         </Container>
       </TouchableWithoutFeedback>
