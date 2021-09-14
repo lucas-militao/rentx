73c7cfd2a600c79e1d13086c4bee3415be931d07
lumilitas <lucasmengineer@edu.unifor.br>
2021-09-14 16:13:29 -0300

    Implementada confirmação de signOut

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -97,6 +97,23 @@ export function Profile() {
     }    
   }
 
+  async function handleSignOut() {
+    Alert.alert('Tem certeza?',
+      'Se você sair, irá precisar de internet para conectar-se novamente.',
+      [
+        {
+          text: 'Cancelar',
+          onPress: () => {},
+          style: 'cancel'
+        },
+        {
+          text: "Sair",
+          onPress: () => signOut()
+        }
+      ]
+    );
+  }
+
   return(
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -105,7 +122,7 @@ export function Profile() {
             <HeaderTop>
               <BackButton color={theme.colors.shape} onPress={handleBack}/>
               <HeaderTitle>Editar Perfil</HeaderTitle>
-              <LogoutButton onPress={signOut}>
+              <LogoutButton onPress={handleSignOut}>
                 <Feather 
                   name="power"
                   size={24}
